8ce4dd537278f0b019d72d0bd207ffeb5ca68cf6
abdussamettrkr <abdussamettrkr@gmail.com>
2021-03-14 21:19:01 +0300

    Bug fixes

--- src/main/java/myDemoApp/App.java
+++ src/main/java/myDemoApp/App.java
@@ -25,10 +25,9 @@ public class App {
     public static void main(String[] args) {  
         
         Logger logger = LogManager.getLogger(App.class);
-        int port = Integer.parseInt(System.getenv("PORT"));
-        port(port);
-        logger.error("Current port number:" + port);
         port(getHerokuAssignedPort());
+
+        
         
 
         get("/", (req, res) -> "Hello, World");
@@ -105,6 +104,15 @@ public class App {
         return result;
 
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 
     
 }
